40a54a78a6c8393bd3a50fa0cb47fa35af257a64
maxvanderschee <m.v.d.schee@ewake.nl>
2017-02-07 10:48:10 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="nl">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="robots" content="index, follow" />
+		<title>The Netherlands Second</title>
+		<link rel="stylesheet" href="css/master.css">
+	</head>
+    <body>
+        <main class="main">
+            <p>hello, world.</p>
+        </main>
+        <footer class="footer">
+
+        </footer>
+        <script src="js/master.js"></script>
+  </body>
+</html>
+
+<?php
+
+?>
